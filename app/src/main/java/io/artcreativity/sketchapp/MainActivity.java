9c49ca959fef922d00e09da7493da93576407143
ZANNOU Judicael <madrojudi.gz@gmail.com>
2022-10-12 15:25:44 +0100

    scope: add ListView

--- app/src/main/java/io/artcreativity/sketchapp/MainActivity.java
+++ app/src/main/java/io/artcreativity/sketchapp/MainActivity.java
@@ -3,13 +3,28 @@ package io.artcreativity.sketchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView productListView;
 
     @Override // premiere fonction appelee
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        productListView = findViewById(R.id.product_list);
+        List<String> strs = new ArrayList<>();
+        for (int i=1; i<100; i++)  {
+            strs.add(String.valueOf(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.simple_product, strs);
+        productListView.setAdapter(adapter);
     }
     @Override // appelee apres le onCreate | Rappelee apres onStop si activite non detruite
     protected void onStart() {
